b056250aafb5f636dcf3e9999c3ab238a777b11e
二狗 <87976956@qq.com>
2021-01-19 02:56:11 +0800

    商品分享

--- src/servers/baseUrl.js
+++ src/servers/baseUrl.js
@@ -2,8 +2,8 @@ const getBaseUrl = () => {
   let BASE_URL = '';
   if (process.env.NODE_ENV === 'development') {
     //开发环境 - 根据请求不同返回不同的BASE_URL
-    // BASE_URL = 'https://crm.ipicture.vip/api'
-    BASE_URL = 'http://crm.test/api'
+    BASE_URL = 'https://crm.ipicture.vip/api'
+    // BASE_URL = 'http://crm.test/api'
   } else {
     BASE_URL = 'https://crm.ipicture.vip/api'
   }
